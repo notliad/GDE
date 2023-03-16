4e5c365afc4060a795d01c46949fde1b9ba2c3c0
Philipe <philipeyuri@gmail.com>
2023-03-16 03:17:26 -0300

    Fix join team and leave room

---
+++ Assets/Scripts/Consts/PhotonRaiseEventExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Photon.Realtime;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.Consts
+{
+    public class PhotonRaiseEvent
+    {
+        public static RaiseEventOptions ToAll { get => new RaiseEventOptions { Receivers = ReceiverGroup.All }; }
+    }
+}
---
+++ Assets/Scripts/Consts/Teams.cs
@@ -0,0 +1,12 @@
+﻿using Photon.Pun.UtilityScripts;
+
+namespace Assets.Scripts.Consts
+{
+    public class Teams
+    {
+        public static PhotonTeam Team_1 { get; private set; } = new PhotonTeam { Code = (byte)1 };
+        public static PhotonTeam Team_2 { get; private set; } = new PhotonTeam { Code = (byte)2 };
+
+        public static PhotonTeam GetTeam(int team) => team == 1 ? Team_1 : Team_2;
+    }
+}
